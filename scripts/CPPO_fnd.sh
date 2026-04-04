5f551911350009fcca3f594eb2aeaec46c57d4ed
fenglin <1349410043@qq.com>
2026-04-04 19:56:30 +0800

    为第一次运行debug

--- scripts/CPPO_fnd.sh
+++ scripts/CPPO_fnd.sh
@@ -8,4 +8,5 @@ accelerate launch  --config_file recipes/accelerate_configs/zero3.yaml \
     --eval_steps=100 --max_completion_length=1024 \
     --model_name_or_path=./models/Qwen3-0.6B \
     --dataset_name=data/liar-raw \
-    --num_generations=16 
+    --num_generations=16 \
+    --log_level=DEBUG
