7702b975a1b96051016e35776b927f36b73de1e1
fenglin <1349410043@qq.com>
2026-04-05 11:47:36 +0800

    优化了整体训练逻辑

--- test/test_vllm.py
+++ test/test_vllm.py
@@ -6,12 +6,13 @@ print("CUDA_VISIBLE_DEVICES =", os.environ.get("CUDA_VISIBLE_DEVICES"))
 print("torch.cuda.is_available() =", torch.cuda.is_available())
 print("torch.cuda.device_count() =", torch.cuda.device_count())
 
-for i in range(torch.cuda.device_count()):
-    print("device", i, torch.cuda.get_device_name(i))
-    print("mem_get_info(i) =", torch.cuda.mem_get_info(i))
+assert torch.cuda.device_count() == 1
+print("device 0 =", torch.cuda.get_device_name(0))
+print("mem_get_info(0) =", torch.cuda.mem_get_info(0))
 
-vllm_device = "cuda:2"   # 这里一定要写“进程内本地索引”
-torch.cuda.set_device(vllm_device)
+torch.cuda.set_device(0)
+x = torch.zeros(1, device="cuda:0")
+print("tensor device =", x.device)
 print("current_device =", torch.cuda.current_device())
 print("current mem_get_info =", torch.cuda.mem_get_info())
 
@@ -19,9 +20,11 @@ llm = LLM(
     model="./models/Qwen3-0.6B",
     tensor_parallel_size=1,
     trust_remote_code=True,
-    device=vllm_device,
+    device="cuda",
     model_impl="transformers",
-    gpu_memory_utilization=0.5
+    gpu_memory_utilization=0.5,
+    distributed_executor_backend="uni",
+    enforce_eager=True,
 )
 
 outs = llm.generate(["Hello"], SamplingParams(max_tokens=8))
