b2e6dff64a31b87d6049997020c7cc6b0aefe558
fenglin <1349410043@qq.com>
2026-04-05 11:27:58 +0800

    优化了整体训练逻辑

---
+++ test/test_gpu.py
@@ -0,0 +1,15 @@
+import os
+import torch
+
+print("CUDA_VISIBLE_DEVICES =", os.environ.get("CUDA_VISIBLE_DEVICES"))
+print("torch.cuda.is_available() =", torch.cuda.is_available())
+print("torch.cuda.device_count() =", torch.cuda.device_count())
+
+for i in range(torch.cuda.device_count()):
+    print("device", i, torch.cuda.get_device_name(i))
+    print("mem_get_info(i) =", torch.cuda.mem_get_info(i))
+
+vllm_device = "cuda:2"   # 这里一定要写“进程内本地索引”
+torch.cuda.set_device(vllm_device)
+print("current_device =", torch.cuda.current_device())
+print("current mem_get_info =", torch.cuda.mem_get_info())
