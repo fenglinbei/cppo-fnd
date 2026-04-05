86ef5a13ff603a0428cee6328b24f45025bf50e2
fenglin <1349410043@qq.com>
2026-04-05 20:24:28 +0800

    grpo debug

--- scripts/CPPO_fnd.sh
+++ scripts/CPPO_fnd.sh
@@ -4,11 +4,11 @@ accelerate launch  --config_file recipes/accelerate_configs/zero2.yaml \
     --num_processes=3  \
     --main_process_port 10909 \
     src/open_r1/grpo_fnd.py \
-    --config recipes/fnd/Qwen3-0.6B.yaml \
+    --config recipes/fnd/Qwen2.5-1.5B-Instruct.yaml \
     --output_dir=output/fnd/ \
     --save_strategy='best' \
     --eval_steps=100 --max_completion_length=1024 \
-    --model_name_or_path=./models/Qwen3-0.6B \
+    --model_name_or_path=./models/Qwen2.5-1.5B-Instruct \
     --dataset_name=data/liar-raw \
     --num_generations=12 \
     --log_level=info \
