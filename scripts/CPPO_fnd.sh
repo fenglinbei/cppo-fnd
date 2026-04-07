25cb75ad055f069fd2e44b875d0eba565546e9cb
fenglin <1349410043@qq.com>
2026-04-07 12:58:02 +0800

    grpo debug

--- scripts/CPPO_fnd.sh
+++ scripts/CPPO_fnd.sh
@@ -1,9 +1,9 @@
 export WANDB_PROJECT="factcheck-grpo"
 export WANDB_API_KEY="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"
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2
 export CUDA_LAUNCH_BLOCKING=1
 accelerate launch  --config_file recipes/accelerate_configs/zero2.yaml \
-    --num_processes=3  \
+    --num_processes=2  \
     --main_process_port 10909 \
     src/open_r1/grpo_fnd.py \
     --config recipes/fnd/Qwen2.5-1.5B-Instruct.yaml \
@@ -12,6 +12,6 @@ accelerate launch  --config_file recipes/accelerate_configs/zero2.yaml \
     --eval_steps=100 --max_completion_length=1024 \
     --model_name_or_path=./models/Qwen2.5-1.5B-Instruct \
     --dataset_name=data/liar-raw \
-    --num_generations=12 \
+    --num_generations=8 \
     --log_level=info \
     --dataset_train_split=train
