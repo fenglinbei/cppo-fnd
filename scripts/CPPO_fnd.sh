98f2a968c1dca512564a4dbdc2da16b8252c48f6
fenglin <1349410043@qq.com>
2026-04-07 16:54:52 +0800

    vllm debug

--- scripts/CPPO_fnd.sh
+++ scripts/CPPO_fnd.sh
@@ -1,8 +1,8 @@
 export WANDB_PROJECT="factcheck-grpo"
 export WANDB_API_KEY="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"
+export CUDA_DEVICE_ORDER="00000000:17:00.0，00000000:19:00.0，00000000:1A:00.0，00000000:98:00.0"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUDA_LAUNCH_BLOCKING=1
-export CUDA_DEVICE_ORDER="00000000:17:00.0"
 accelerate launch  --config_file recipes/accelerate_configs/zero2.yaml \
     --num_processes=3  \
     --main_process_port 10909 \
